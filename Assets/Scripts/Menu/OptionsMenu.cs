6858b139fe77144e80a78406fc9d4a7f9555b2df
Pedro Belluzzo <pedrobelluzzo@gmail.com>
2020-09-10 23:06:17 -0300

    Initial commit

---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private Slider gameSessionTimeSlider;
+    [SerializeField] private Slider enemyRespawnTimeSlider;
+
+    public void Awake()
+    {
+        LoadPlayerPrefsOnSlider();
+    }
+    public void SavePlayerPrefs()
+    {
+        float gameSessionTime = gameSessionTimeSlider.value;
+        float enemyRespawnTime = enemyRespawnTimeSlider.value;
+        PlayerPrefs.SetFloat("GameSessionTime", gameSessionTime);
+        PlayerPrefs.SetFloat("EnemyRespawnTime", enemyRespawnTime);
+    }
+    public void LoadPlayerPrefsOnSlider()
+    {
+        gameSessionTimeSlider.value = PlayerPrefs.GetFloat("GameSessionTime", 1f);
+        enemyRespawnTimeSlider.value = PlayerPrefs.GetFloat("EnemyRespawnTime", 1f);
+    }
+}
---
+++ Assets/Scripts/Menu/SliderText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SliderText : MonoBehaviour
+{
+    public void UpdateText(float value)
+    {
+        GetComponent<TextMeshProUGUI>().text = value.ToString();
+    }
+}
